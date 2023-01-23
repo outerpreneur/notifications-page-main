7972a45b035462b7f0f2237034abd4d45d6fe87e
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-01-23 14:34:12 +0100

    javascript added

---
+++ main.js
@@ -0,0 +1,26 @@
+const markAsRead = document.querySelector('.mark-read')
+const notificationCounter = document.getElementById('notification-counter')
+const notificationTag = document.querySelectorAll(".user-notification")
+const unreadMessages = document.querySelectorAll('.unread');
+const notificationTagText = notificationTag.innerText
+const notificationTagTextNumber = unreadMessages.length;
+
+
+// // // On clicking each notification, remove unread styles and update
+
+notificationTag.forEach((message) => {
+    message.addEventListener('click', () => {
+        message.classList.toggle('unread')
+        const newUnreadMessages = document.querySelectorAll(".unread")
+        notificationCounter.innerText = newUnreadMessages.length
+    })
+})
+
+// // on clicking mark as read, remove unread styles
+
+notificationTag.forEach(notification => {
+    markAsRead.addEventListener('click', () => {
+        notification.classList.remove('unread');
+        notificationCounter.innerText = 0
+    })
+})
